06dedb25cb43de7cc8e74fb7a6399558b72971fe
Z <45545234+zacharimayer@users.noreply.github.com>
2022-07-24 17:15:43 -0400

    Init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Caeser-Cipher
 Basic Caeser Cipher for encryption written in python. 
+
+Run:
+
+```python 3main.py```
---
+++ main.py
@@ -0,0 +1,47 @@
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+from art import logo
+
+print(logo)
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        elif char not in alphabet:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+should_continue = True
+
+while should_continue == True:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if shift > 26:
+        shift == round(((shift / 26) - 1) * 26)
+    else:
+        shift == shift
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    start_over = input(
+        "Type 'yes' if you want to go again. Otherwise type 'no'. ")
+
+    if start_over == "no":
+        should_continue = False
+        print("Goodbye!")
